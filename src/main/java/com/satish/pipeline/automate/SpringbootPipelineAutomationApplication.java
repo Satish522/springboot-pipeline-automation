e6f26bc0d65ae6352cc27875e71d696c059eff3d
Satish522 <satishsubudhi522@gmail.com>
2023-12-26 14:48:27 +0530

    initial commit

---
+++ src/main/java/com/satish/pipeline/automate/SpringbootPipelineAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.satish.pipeline.automate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPipelineAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPipelineAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satish/pipeline/automate/SpringbootPipelineAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satish.pipeline.automate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPipelineAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
